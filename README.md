84bb0bffc4d11c2f574ac79e04a207514d44fb95
Bryan Müller <bmuller0975@gmail.com>
2022-05-25 23:21:01 -0600

    better seed and db scripts

--- README.md
+++ README.md
@@ -1,18 +1,28 @@
-# Welcome to Remix!
+# Hi User I'm Dad
 
 - [Remix Docs](https://remix.run/docs)
 
-Hi User I'm Dad
+## Setup
 
-- Entry Modal 
-  - Whats your name? 
+```shell
+pnpm install
+
+pnpm run db:prepare
+
+pnpm run build
+```
+
+## Features Ideas
+
+- Entry Modal
+  - Whats your name?
   - Cookie
 - My Jokes
-    - Add via random generator (icanhazdadjokes API?)
-    - Submit own
-    - Persist your backlog of jokes
-    - Submit to Tuesday Queue
+  - Add via random generator (icanhazdadjokes API?)
+  - Submit own
+  - Persist your backlog of jokes
+  - Submit to Tuesday Queue
 - Tuesday Joke Queue
-  - Past delivered w/ ranking 
-  - Queued jokes 
+  - Past delivered w/ ranking
+  - Queued jokes
     - Joke blurred if current user not submitter w/ submitter name visible always
--- prisma/seed.js
+++ prisma/seed.js
@@ -2,17 +2,21 @@ const { PrismaClient } = require("@prisma/client");
 const db = new PrismaClient();
 
 async function seed() {
-  await db.joke.create({
-    data: {
-      externalId: null,
-      delivered: false,
-      content:
-        "Why does a chicken coop only have two doors? Because if it had four doors it would be a chicken sedan.",
-      submitter: {
-        create: {
-          email: "bryan.muller@rms.com",
-          displayName: "Bryan Müller",
-        },
+  await db.user.upsert({
+    where: { email: "bryan.muller@rms.com" },
+    update: {},
+    create: {
+      email: "bryan.muller@rms.com",
+      displayName: "Bryan Müller",
+      jokes: {
+        create: [
+          {
+            externalId: null,
+            delivered: false,
+            content:
+              "Why does a chicken coop only have two doors? Because if it had four doors it would be a chicken sedan.",
+          },
+        ],
       },
     },
   });
