e204ba61a73d6b4bd8ba73bb221e397f4717ec54
Justin Waite <justindwaite@gmail.com>
2022-05-27 09:55:06 -0600

    Use the netlify template instead

--- app/root.tsx
+++ app/root.tsx
@@ -1,13 +1,6 @@
 import type { LinksFunction, MetaFunction } from "@remix-run/node";
-import {
-  Links,
-  LiveReload,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
-} from "@remix-run/react";
-import styles from "./styles/app.css";
+import { Links, LiveReload, Meta, Outlet, Scripts, ScrollRestoration } from "@remix-run/react";
+import styles from "./tailwind.css";
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
