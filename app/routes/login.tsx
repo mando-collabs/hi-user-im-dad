436bf9ec93c82904eb05f8a01bc1c7c1461c6c3b
Justin Waite <justindwaite@gmail.com>
2022-05-27 14:33:51 -0600

    Add eslint configuration for tailwind

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -17,8 +17,8 @@
 export default function LoginPage() {
   return (
     <>
-      <div className="min-h-full flex">
-        <div className="flex-1 flex flex-col justify-center py-12 px-4 sm:px-6 lg:flex-none lg:px-20 xl:px-24">
+      <div className="flex min-h-full">
+        <div className="flex flex-col flex-1 justify-center py-12 px-4 sm:px-6 lg:flex-none lg:px-20 xl:px-24">
           <div className="mx-auto w-full max-w-sm lg:w-96">
             <h2 className="mt-6 text-3xl font-extrabold text-gray-900">🪵 in to your account</h2>
 
@@ -35,7 +35,7 @@ export default function LoginPage() {
                       type="email"
                       autoComplete="email"
                       required
-                      className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-teal-500 focus:border-teal-500 sm:text-sm"
+                      className="block py-2 px-3 w-full placeholder:text-gray-400 rounded-md border border-gray-300 focus:border-teal-500 focus:outline-none focus:ring-teal-500 shadow-sm appearance-none sm:text-sm"
                     />
                   </div>
                 </div>
@@ -59,7 +59,7 @@ export default function LoginPage() {
                 <div>
                   <button
                     type="submit"
-                    className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-teal-600 hover:bg-teal-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-teal-500"
+                    className="flex justify-center py-2 px-4 w-full text-sm font-medium text-white bg-teal-600 hover:bg-teal-700 rounded-md border border-transparent focus:outline-none focus:ring-2 focus:ring-teal-500 focus:ring-offset-2 shadow-sm"
                   >
                     Sign in
                   </button>
@@ -68,9 +68,9 @@ export default function LoginPage() {
             </div>
           </div>
         </div>
-        <div className="hidden lg:block relative w-0 flex-1">
+        <div className="hidden relative flex-1 w-0 lg:block">
           <img
-            className="absolute inset-0 h-full w-full object-cover"
+            className="object-cover absolute inset-0 w-full h-full"
             src="https://images.unsplash.com/photo-1507808973436-a4ed7b5e87c9?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1908"
             alt=""
           />
