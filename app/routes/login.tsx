15a7e62f475b1d56d3835be0480371b0a062bb7a
Justin Waite <justindwaite@gmail.com>
2022-05-26 18:14:17 -0600

    Add a login page

---
+++ app/routes/login.tsx
@@ -0,0 +1,81 @@
+/*
+  This example requires Tailwind CSS v2.0+ 
+  
+  This example requires some changes to your config:
+  
+  ```
+  // tailwind.config.js
+  module.exports = {
+    // ...
+    plugins: [
+      // ...
+      require('@tailwindcss/forms'),
+    ],
+  }
+  ```
+*/
+export default function LoginPage() {
+  return (
+    <>
+      <div className="min-h-full flex">
+        <div className="flex-1 flex flex-col justify-center py-12 px-4 sm:px-6 lg:flex-none lg:px-20 xl:px-24">
+          <div className="mx-auto w-full max-w-sm lg:w-96">
+            <h2 className="mt-6 text-3xl font-extrabold text-gray-900">🪵 in to your account</h2>
+
+            <div className="mt-6">
+              <form action="#" method="POST" className="space-y-6">
+                <div>
+                  <label htmlFor="email" className="block text-sm font-medium text-gray-700">
+                    Email address
+                  </label>
+                  <div className="mt-1">
+                    <input
+                      id="email"
+                      name="email"
+                      type="email"
+                      autoComplete="email"
+                      required
+                      className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-teal-500 focus:border-teal-500 sm:text-sm"
+                    />
+                  </div>
+                </div>
+
+                {/*<div className="space-y-1">*/}
+                {/*  <label htmlFor="password" className="block text-sm font-medium text-gray-700">*/}
+                {/*    Password*/}
+                {/*  </label>*/}
+                {/*  <div className="mt-1">*/}
+                {/*    <input*/}
+                {/*      id="password"*/}
+                {/*      name="password"*/}
+                {/*      type="password"*/}
+                {/*      autoComplete="current-password"*/}
+                {/*      required*/}
+                {/*      className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-teal-500 focus:border-teal-500 sm:text-sm"*/}
+                {/*    />*/}
+                {/*  </div>*/}
+                {/*</div>*/}
+
+                <div>
+                  <button
+                    type="submit"
+                    className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-teal-600 hover:bg-teal-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-teal-500"
+                  >
+                    Sign in
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+        <div className="hidden lg:block relative w-0 flex-1">
+          <img
+            className="absolute inset-0 h-full w-full object-cover"
+            src="https://images.unsplash.com/photo-1507808973436-a4ed7b5e87c9?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1908"
+            alt=""
+          />
+        </div>
+      </div>
+    </>
+  );
+}
