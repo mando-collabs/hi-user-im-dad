cdd21da6a7c8f0ff8cce69d23989c0ee756ce101
Justin Waite <justindwaite@gmail.com>
2022-05-27 09:55:06 -0600

    Use the netlify template instead

--- remix.config.js
+++ remix.config.js
@@ -2,10 +2,11 @@
  * @type {import('@remix-run/dev').AppConfig}
  */
 module.exports = {
+  serverBuildTarget: "netlify",
+  server: "./server.js",
   ignoredRouteFiles: ["**/.*"],
-  devServerPort: 8002,
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
-  // serverBuildPath: "build/index.js",
+  // serverBuildPath: ".netlify/functions-internal/server.js",
   // publicPath: "/build/",
 };
