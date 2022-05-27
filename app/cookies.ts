f6d43eceba84b443133c8bff3f53276211db7581
Justin Waite <justindwaite@gmail.com>
2022-05-27 07:38:45 -0600

    Delete unused cookies file

--- app/cookies.ts
+++
@@ -1,5 +0,0 @@
-import { createCookie } from "@remix-run/node"; // or "@remix-run/cloudflare"
-
-export const userCookie = createCookie("user", {
-  expires: new Date(Date.now() + 1000 * 60 * 60 * 24), // expires in 24 hours
-});
