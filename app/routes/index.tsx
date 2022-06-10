ee3e8267f4a43233a55bdc089f661567d505e7f2
Justin Waite <justindwaite@gmail.com>
2022-06-10 15:31:38 -0600

    style the main joke queue

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -9,12 +9,23 @@ import { RandomJoke } from "~/components/RandomJoke";
 import { db } from "~/utils/db.server";
 import { Joke } from "@prisma/client";
 import { PlaceholderAvatar } from "~/components/PlaceholderAvatar";
+import classNames from "classnames";
+import { any } from "~/utils/any.server";
+import { Button } from "@justinwaite/tailwind-ui";
+import { CheckIcon } from "@heroicons/react/outline";
 
 interface LoaderData {
   username: string;
   profileImgUrl: string | null;
   randomJoke: DadJokeApiResponse;
-  jokeQueue: { id: number; content: string; userImg: string | null; username: string }[];
+  jokeQueue: {
+    id: number;
+    content: string;
+    userImg: string | null;
+    username: string;
+    delivered: boolean;
+    isMyJoke: boolean;
+  }[];
   myJokes: Joke[];
 }
 
@@ -27,7 +38,7 @@ export const loader: LoaderFunction = async ({ request }) => {
 
   const randomJoke = await JokeService.generateRandomJoke();
   const jokeQueue = await db.joke.findMany({
-    take: 10,
+    take: 15,
     orderBy: { createdAt: "desc" },
     include: { submitter: { select: { displayName: true, profileImgUrl: true } } },
   });
@@ -41,12 +52,17 @@ export const loader: LoaderFunction = async ({ request }) => {
     username: user.displayName,
     profileImgUrl: user.profileImgUrl,
     randomJoke,
-    jokeQueue: jokeQueue.map((joke) => ({
-      id: joke.id,
-      content: joke.content,
-      username: joke.submitter.displayName,
-      userImg: joke.submitter.profileImgUrl,
-    })),
+    jokeQueue: jokeQueue.map((joke) => {
+      let isMyJoke = joke.submitterId === user.id;
+      return {
+        id: joke.id,
+        content: joke.delivered || isMyJoke ? joke.content : any.sentence(),
+        username: joke.submitter.displayName,
+        userImg: joke.submitter.profileImgUrl,
+        delivered: joke.delivered,
+        isMyJoke,
+      };
+    }),
     myJokes,
   };
 
@@ -78,18 +94,32 @@ export default function Index() {
             </div>
             {/* End main area */}
           </main>
-          <aside className="hidden overflow-y-auto relative shrink-0 w-full max-w-2xl border-r border-gray-200 xl:flex xl:flex-col xl:order-first">
+          <aside className="hidden overflow-hidden relative shrink-0 w-full max-w-2xl border-r border-gray-200 xl:flex xl:flex-col xl:order-first">
             {/* Start secondary column (hidden on smaller screens) */}
             <div className="absolute inset-0 py-6 px-4 sm:px-6 lg:px-8">
-              <div className="h-full rounded-lg border-2 border-gray-200 border-dashed space-y-4 p-4">
+              <div className="h-full space-y-4 p-4 overflow-y-auto">
+                <h1 className="text-3xl font-extrabold">The Joke Queue</h1>
                 {jokeQueue.map((joke) => (
-                  <div key={joke.id} className="flex items-start">
+                  <div key={joke.id} className="flex items-start text-gray-700 border rounded-md px-4 py-2">
                     {joke.userImg ? (
-                      <img className="h-6 w-6 rounded-full" src={joke.userImg} />
+                      <img
+                        className="h-6 w-6 rounded-full"
+                        src={joke.userImg}
+                        alt={joke.username}
+                        title={joke.username}
+                      />
                     ) : (
                       <PlaceholderAvatar className="h-6 w-6" />
                     )}
-                    <div className="ml-2">{joke.content}</div>
+                    <div className={classNames("ml-2", { blur: !joke.delivered && !joke.isMyJoke })}>
+                      {joke.content}
+                    </div>
+                    <span className="flex-1" />
+                    {joke.isMyJoke ? (
+                      <Button kind="white" size="xs" className="flex-shrink-0" leadingIcon={CheckIcon}>
+                        Delivered
+                      </Button>
+                    ) : null}
                   </div>
                 ))}
               </div>
