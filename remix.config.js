e468919e6d9292a6713b2fd7a5a3363ba236bcd2
Justin Waite <justindwaite@gmail.com>
2022-06-23 13:15:53 -0600

    switch to fly.io for hosting

--- remix.config.js
+++ remix.config.js
@@ -1,14 +1,9 @@
-/**
- * @type {import('@remix-run/dev').AppConfig}
- */
+/** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
-  serverBuildTarget: "netlify",
-  server: "./server.js",
-  // https://stackoverflow.com/questions/12632029/grunt-minimatch-glob-folder-exclusion
-  ignoredRouteFiles: ["**/.*", "**/*.test.tsx?", "**/feature/**/*"],
+  ignoredRouteFiles: ["**/.*"],
   devServerPort: 8002,
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
-  // serverBuildPath: ".netlify/functions-internal/server.js",
+  // serverBuildPath: "build/index.js",
   // publicPath: "/build/",
 };
